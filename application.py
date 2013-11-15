336daa0b4a87c1617494a31768cd7cfb64b4aa1c
Eli Gundry <eligundry@gmail.com>
2013-11-15 17:50:36 -0500

    Added leaflet and started working with websockets and twitter

--- application.py
+++ application.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_sockets import Sockets
+from twython import Twython, TwythonStreamer
 
 app = Flask(__name__)
+sockets = Sockets(app)
+
+# Twitter API constants
+TA = {
+    'ck': "HRhPFogV5kE23OnVY88Fw",
+    'cs': "HUxEKIP51KJEfg6TVqzTcLT5mYjAkW9IV6XThrBDZM",
+    'atk': "1965992269-onDizJpLZEcBBcPWbSGof0FnI0U2TlU9YI44n0K",
+    'ats': "D5UK4AoB89AvbfrejZJYDMKvk94sdsiyIyUKBHBdfk"
+}
+
+class TweetStreamer(TwythonStreamer):
+    def on_success(self, data):
+        return data['text']
+
+    def on_error(self, status_code, error):
+        print("%s: %s", status_code, error)
+        self.disconnect()
+
+def setup_twitter():
+   return TweetStreamer(TA['ck'], TA['cs'], TA['atk'], TA['ats'])
 
 @app.route('/')
 def root():
     return render_template('map.html')
 
+@sockets.route('/tweets')
+def get_tweets(ws):
+    query = {
+        "locations": "41.1367915,-81.389365,41.161646,-81.3413963"
+    }
+
+    a = setup_twitter()
+
+    while True:
+        ws.send(a.statuses.filter(locations = query["locations"]))
+
 if __name__ == "__main__":
+    app.debug = True
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,20 @@ var leaflet_config = {
 	key: "2ca42758fa584876a09900fc5b401400"
 };
 
+var get_tweets = function() {
+	var tweets = new WebSocket("ws://" + window.location.host + "/tweets");
+
+	tweets.onopen = function() {
+		console.log("Connection open!");
+	}
+
+	tweets.onmessage = function(msg) {
+		console.log(msg);
+	}
+
+	return tweets;
+};
+
 $(function() {
 	// Make the map full screen
 	$("#map").css({
@@ -11,8 +25,13 @@ $(function() {
 	});
 
 	var map = L.map('map').setView([41.150556,-81.361111], 13);
+	var markerLayer = new L.LayerGroup();
 
 	L.tileLayer('http://{s}.tile.cloudmade.com/' + leaflet_config.key + '/' + leaflet_config.style + '/256/{z}/{x}/{y}.png', {
 		maxZoom: 18
 	}).addTo(map);
+
+	var heatmap = new L.TileLayer.HeatCanvas({}, {});
+
+	map.addLayer(heatmap);
 });
