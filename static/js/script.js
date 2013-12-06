f3a7fda388b45716a2f580cff2b53033d1db42c0
Eli Gundry <eligundry@gmail.com>
2013-12-06 17:51:18 -0500

    Finally got the heatmap working! WEBGL FTW!!!

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,3 @@
-var leaflet_config = {
-	style: 997,
-	key: "2ca42758fa584876a09900fc5b401400"
-};
-
 var get_tweets = function() {
 	var tweets = new WebSocket("ws://" + window.location.host + "/tweets");
 
@@ -27,23 +22,30 @@ $(function() {
 	L.Icon.Default.imagePath = '/static/images'
 
 	// Set inital view in Kent, Ohio
-	var baseLayer = new L.TileLayer('http://{s}.tile.cloudmade.com/' + leaflet_config.key + '/' + leaflet_config.style + '/256/{z}/{x}/{y}.png', {
+	var baseLayer = new L.TileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
+		styleId: 997,
+		key: "2ca42758fa584876a09900fc5b401400",
 		maxZoom: 18
 	});
 
 	var markerLayer = new L.LayerGroup();
 
+	var heatmap = new L.TileLayer.WebGLHeatMap({
+		autoresize: true,
+		opacity: 1,
+		size: 5000
+	});
+
 	var map = new L.Map('map', {
 		center: [41.150556, -81.361111],
 		zoom: 14,
-		layers: [baseLayer, markerLayer]
+		layers: [baseLayer, markerLayer, heatmap]
 	});
 
 	var tweet_stream = get_tweets();
 
 	tweet_stream.onmessage = function(event) {
 		var t = jQuery.parseJSON(event.data);
-		console.log(t);
 
 		if (t.disconnect) {
 			tweet_stream.close();
@@ -54,5 +56,9 @@ $(function() {
 			.marker([t.latitude, t.longitude])
 			.bindPopup(t.html)
 			.addTo(markerLayer);
+
+		heatmap.addDataPoint(t.latitude, t.longitude, 10);
+		heatmap.update();
+		console.log(heatmap)
 	};
 });
